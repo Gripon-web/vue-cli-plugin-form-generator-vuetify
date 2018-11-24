588aeb7d9746ce1fe41201e694be09b37f928bb9
Grégoire Gripon <gripon-web@outlook.fr>
2018-11-24 11:15:54 +0100

    update version

--- generator/tools/formGenerator.js
+++ generator/tools/formGenerator.js
@@ -3,7 +3,7 @@ const helpers = require('./helpers')
 function addDependencies(api) {
   api.extendPackage({
     dependencies: {
-      "form-generator-vuetify": "^0.1.4"
+      "form-generator-vuetify": "^0.1.5"
     }
   })
 }
